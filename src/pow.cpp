d2e8ce87337dd96993bc26d26a96fd70bf024ea9
lestreyd <flestreyd@gmail.com>
2018-03-11 02:43:35 +0300

    Message about nBits

--- src/pow.cpp
+++ src/pow.cpp
@@ -125,8 +125,8 @@ bool CheckProofOfWork(uint256 hash, unsigned int nBits)
         return error("CheckProofOfWork() : nBits below minimum work");
 
     // Check proof of work matches claimed amount
-    if (hash > bnTarget)
-        return error(strcat("CheckProofOfWork() : hash doesn't match nBits < ", hash.ToString().c_str()));
+    if (hash > bnTarget || hash < bnTarget)
+        return error(strcat("CheckProofOfWork() : hash doesn't match nBits: ", hash.ToString().c_str()));
 
     return true;
 }
