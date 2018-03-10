298e03a69da1a94900cdea6f9bb94e2e068726b8
lestreyd <flestreyd@gmail.com>
2018-03-10 04:24:42 +0300

    c++ strcat adding

--- src/pow.cpp
+++ src/pow.cpp
@@ -122,11 +122,11 @@ bool CheckProofOfWork(uint256 hash, unsigned int nBits)
 
     // Check range
     if (fNegative || bnTarget == 0 || fOverflow || bnTarget > Params().ProofOfWorkLimit())
-        return error("CheckProofOfWork() : nBits below minimum work - " + bnTarget.ToString().c_str());
+        return error(strcat("CheckProofOfWork() : nBits below minimum work - ", bnTarget.ToString().c_str()));
 
     // Check proof of work matches claimed amount
     if (hash > bnTarget)
-        return error("CheckProofOfWork() : hash doesn't match nBits < " + hash.ToString().c_str());
+        return error(strcat("CheckProofOfWork() : hash doesn't match nBits < ", hash.ToString().c_str()));
 
     return true;
 }
